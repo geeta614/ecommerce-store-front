d0a62b2cd8a5674e67839f6fd7329673c9265ea4
geeta614 <geeta.614.garg@gmail.com>
2026-04-06 00:11:58 +0530

    Update CategoryController.java

--- productcatalog/src/main/java/com/ecom/productcatalog/controller/CategoryController.java
+++ productcatalog/src/main/java/com/ecom/productcatalog/controller/CategoryController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://ecommstorefront.netlify.app/")
 public class CategoryController {
 
     private final CategoryService categoryService;
