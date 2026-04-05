cf531c9f944cf57449d67e449d05e103e3107eb4
geeta614 <geeta.614.garg@gmail.com>
2026-04-06 00:16:08 +0530

    Update ProductList.jsx

--- ecom-catalog-react/src/ProductList.jsx
+++ ecom-catalog-react/src/ProductList.jsx
@@ -11,7 +11,7 @@ const ProductList = ({ products }) => {
                         <div className="card-body">
                             <h5 className="card-title">{product.name}</h5>
                             <p className="card-text">{product.description}</p>
-                            <p className="card-text"><strong>${product.price}</strong></p>
+                            <p className="card-text"><strong>Rs.{product.price}</strong></p>
                         </div>
                     </div>
                 </div>
