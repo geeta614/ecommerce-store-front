79a029877169b66328ab8a137b6f28f40470e09d
geeta614 <geeta.614.garg@gmail.com>
2026-04-05 23:48:34 +0530

    Update CategoryController.java

--- productcatalog/src/main/java/com/ecom/productcatalog/controller/CategoryController.java
+++ productcatalog/src/main/java/com/ecom/productcatalog/controller/CategoryController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class CategoryController {
 
     private final CategoryService categoryService;
